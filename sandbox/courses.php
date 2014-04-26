0e152da756daea6ec39b6ca69f505a8fbe2b7cc7
packardbell <nevilgeorge@ubuntu.(none)>
2014-04-26 02:26:41 -0500

    latest changes to courses page

---
+++ sandbox/courses.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Log in</title>
+
+		<link rel="stylesheet" href="packages/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="courses.css">
+	</head>
+
+	<body>
+		<?php include 'header.html'; ?>
+
+		<ul>
+
+			<div class='course'> Course 1</div>
+			<div class='course'> Course 2</div>
+			<div class='course'> Course 3</div>
+			<div class='course'> Course 4</div>
+
+		</ul>
+		
+	</body>
+
+</html>
