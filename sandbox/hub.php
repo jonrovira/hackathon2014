1e324c94fc862944e00c5933b8919450e733e78a
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-04-26 01:17:38 -0500

    decluttered, paper added

---
+++ sandbox/hub.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Feedback</title>
+
+		<link rel="stylesheet" href="packages/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="hub.css">
+	</head>
+
+	<body>
+		<?php include 'header.html'; ?>
+
+		<div id="wrapper">
+			<div id="presets">
+
+			</div>
+			<div id="ask">
+				<h2>Ask a question.</h2>
+			</div>
+		</div>
+	</body>
+
+</html>
