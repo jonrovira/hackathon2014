a99eee57c9550d48a13bc9fc23c63c257c08c123
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-04-25 23:12:56 -0500

    sandbox added

---
+++ sandbox/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Log in</title>
+
+		<link rel="stylesheet" href="packages/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="login.css">
+	</head>
+
+	<body>
+		<?php include 'header.html'; ?>
+
+		<div id="credentials">
+			<input type="text" id="netid" value="Net ID">
+			<input type="text" id="netidpass" value="Password">
+		</div>
+	</body>
+
+</html>
