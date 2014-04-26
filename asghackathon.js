22e66972f0c4460c604116e4d7b43aeb6e1dbe89
petehuang <peteh22@gmail.com>
2014-04-25 22:50:15 -0500

    Messages and default feedback buttons

--- asghackathon.js
+++ asghackathon.js
@@ -2,52 +2,78 @@ Lectures = new Meteor.Collection('lectures');
 Messages = new Meteor.Collection('messages');
 CurrentIndex = 0;
 
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to our ASG Hackathon project.";
-  };
 
-  Template.listLectures.helpers({
-    lectures: function() 
-    { 
-      return Lectures.find(); 
-    }
-  });
+if (Meteor.isClient) {
+  
+  Meteor.subscribe('lectures');
 
   Template.createLecture.events({
     'click #submitLectureName': function() {
       var name = document.getElementById("lectureName").value;
-      var index = Lectures.find().count();
       Lectures.insert({name: name, index: CurrentIndex++});
       document.getElementById("lectureName").value = '';
+    }
+  });
+
+  Template.lectureDetail.events({
+    
+
+    'click #submitNewFeedback': function() {
+      var content = document.getElementById("feedbackContent").value;
+      var currentLecture = Session.get('currentLecture');
+      Messages.insert({content: content, lecture: currentLecture});
+      document.getElementById("feedbackContent").value = '';
+    },
+
+    'click #lectureTooFast': function() {
+      var currentLecture = Session.get('currentLecture');
+      Messages.insert({content: "Please slow down a bit!", lecture: currentLecture});
+    },
+
+    'click #lectureTooSlow': function() {
+      var currentLecture = Session.get('currentLecture');
+      Messages.insert({content: "You can speed up a bit", lecture: currentLecture});
+    },
+    'click #goBackSlide': function() {
+      var currentLecture = Session.get('currentLecture');
+      Messages.insert({content: "Can you go back a slide?", lecture: currentLecture});
+    },
+
+    'click #doNotUnderstandContent': function() {
+      var currentLecture = Session.get('currentLecture');
+      Messages.insert({content: "I don't understand this.", lecture: currentLecture});
     }
   });
 
   Router.map(function() {
 
     this.route('listLectures', {
-      path: '/',  
+      path: '/',
       data: function(){
-        templateData: { 
-          lectures: Lectures.find({}); 
+        templateData = { 
+          lectures: Lectures.find()
         };
+        return templateData;
       }
     });
 
     this.route('lectureDetail', {
+      
       path: '/:index',
-
+      waitOn: function() {
+        var currentLecture = parseInt(this.params.index);
+        return Meteor.subscribe('messages', currentLecture);
+      },
       data: function(){
-
-        var castIndexToInt = parseInt(this.params.index);
-        var currentLecture = Lectures.findOne({index: castIndexToInt});
-        console.log(currentLecture);
+        var currentLecture = parseInt(this.params.index);
+        Session.set('currentLecture', currentLecture);
         templateData = { 
-          name: currentLecture.name,
-          messages: Messages.find({lecture: castIndexToInt})
+          lecture: Lectures.findOne({index: currentLecture}),
+          messages: Messages.find({lecture: currentLecture})
         };
         return templateData;
-      }
+      },
+
     });
   });
 }
@@ -55,5 +81,12 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Meteor.publish('lectures', function() {
+      return Lectures.find();
+    });
+
+    Meteor.publish('messages', function(currentLecture) {
+      return Messages.find({lecture: currentLecture});
+    })
   });
 }
