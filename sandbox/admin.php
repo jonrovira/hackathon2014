7d121f49f06a0dc5c0549a3cd696a3696107bfa0
packardbell <nevilgeorge@ubuntu.(none)>
2014-04-26 06:42:45 -0500

    Front-end for professor in class

---
+++ sandbox/admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Admin</title>
+
+		<link rel="stylesheet" href="packages/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="admin.css">
+	</head>
+
+	<body>
+		<?php include 'header.html'; ?>
+
+		<div id="productname">Title Here</div>
+		
+		<div class="sidepanel">
+				<div class="span4">Response 1</div>
+				<div class="span4">Response 2</div>
+				<div class="span4">Response 3</div>
+				<div class="span4">Response 4</div>
+		</div>
+
+		<div id="livefeed">Livefeed Here</div>
+
+
+	</body>
+
+</html>
